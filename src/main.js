ad5ddbff305ed12ff8b2cf58edf27efcfc72047c
Jeroen van Seeters <vanseetersjeroen@gmail.com>
2021-02-08 12:00:24 -0800

    add weather conditions and convert button
    Co-authored-by: Kerry Lang <klang812@gmail.com>
    Co-authored-by: Tien Nguyen <tien96ng@gmail.com>

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
+// function convertKToF (temp) {
+//   return 1.8 * (temp - 273) +32
+// }
 $(document).ready(function() {
   $('#weatherLocation').click(function() {
     const city = $('#location').val();
@@ -14,6 +17,7 @@ $(document).ready(function() {
     request.onreadystatechange = function() {
       if (this.readyState === 4 && this.status === 200) {
         const response = JSON.parse(this.responseText);
+        console.log(response);
         getElements(response);
       }
     }
@@ -21,9 +25,24 @@ $(document).ready(function() {
     request.open("GET", url, true);
     request.send();
 
-   function getElements(response) {
+    function getElements(response) {
+      $(".celsius-btn").show();
       $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
-      $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
+      $('.showTemp').text(`The temperature in Fahrenheit is ${(1.8 * (response.main.temp - 273) + 32).toFixed(2)} degrees.`);
+      $('.showWeather').text(`The current weather condition is ${response.weather[0].description}! `)
+      $('.showWind').text(`The wind speed is ${2.237 * (response.wind.speed)} MPH.`);
+
+      $(".celsius-btn").click(() => {
+        $(".fahrenheit-btn").show();
+        $(".celsius-btn").hide();
+        $('.showTemp').text(`The temperature in Celsius is ${(response.main.temp - 273).toFixed(2)} degrees.`);
+      });
+  
+      $(".fahrenheit-btn").click(() => {
+        $(".celsius-btn").show();
+        $(".fahrenheit-btn").hide();
+        $('.showTemp').text(`The temperature in Fahrenheit is ${(1.8 * (response.main.temp - 273) + 32).toFixed(2)} degrees.`);
+      });
     }
   });
 });
